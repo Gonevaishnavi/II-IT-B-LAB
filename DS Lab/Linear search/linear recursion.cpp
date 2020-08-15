6f8b302e481ee0e0fcc467c4dfe449e4b82b0b44
Gonevaishnavi <69639426+Gonevaishnavi@users.noreply.github.com>
2020-08-15 13:14:03 +0530

    Add files via upload

---
+++ DS Lab/Linear search/linear recursion.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int ReLinearSearch(int [],int,int);
+int main()
+{
+	int i,position,key;
+	int a[10]= {12,61,33,92,36,3,29,98,54,60};
+	scanf("%d",&key);
+	position = ReLinearSearch(a,10,key);
+	if (position == -1){
+		printf("search is unsucessful");
+	}
+	else{
+		printf("search is sucessful at index %d\n", position);
+	}
+	return 0;
+}
+int ReLinearSearch(int a[],int n,int key)
+{
+	if (n > 0){
+		if(a[n - 1] == key){
+			return n-1;
+		}
+		else{
+			return ReLinearSearch(a,n-1,key);
+		}
+		
+	}	
+	return -1;
+}
+
+
